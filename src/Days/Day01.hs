23488f6c93dc5305a90e645c8889c6edd85e1eac
Thomas Stacey <Thomas.Stacey@warwick.ac.uk>
2022-12-01 08:02:36 +0000

    Day 1

--- src/Days/Day01.hs
+++ src/Days/Day01.hs
@@ -1,19 +1,21 @@
 module Days.Day01 where
-import qualified Program.RunDay as R (runDay)
+import           Data.List       (sort)
+import           Data.List.Split (splitOn)
+import qualified Program.RunDay  as R (runDay)
 
 runDay :: String -> IO (Maybe Integer, Maybe Integer)
 runDay = R.runDay parser part1 part2
 
-type Input = [Int]
+type Input = [[Int]]
 
 type Output1 = Int
 type Output2 = Int
 
 parser :: String -> Input
-parser = undefined
+parser = map (map read . lines) . splitOn "\n\n"
 
 part1 :: Input -> Output1
-part1 = undefined
+part1 = maximum . map sum
 
 part2 :: Input -> Output2
-part2 = undefined
+part2 = sum . take 3 . reverse . sort . map sum
