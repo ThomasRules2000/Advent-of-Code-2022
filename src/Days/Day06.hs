8a3ffac4f7157474f89b23baa8ff31dc43345a0b
Thomas Stacey <Thomas.Stacey@warwick.ac.uk>
2022-12-06 08:33:36 +0000

    Day 6

--- src/Days/Day06.hs
+++ src/Days/Day06.hs
@@ -1,4 +1,7 @@
 module Days.Day06 where
+import           Data.List       (tails)
+import           Data.Set        (Set)
+import qualified Data.Set        as Set
 import qualified Program.RunDay  as R (runDay)
 import qualified Program.TestDay as T (testDay)
 import           System.Clock    (TimeSpec)
@@ -8,18 +11,21 @@ runDay :: String -> IO (Maybe TimeSpec, Maybe TimeSpec, Maybe TimeSpec)
 runDay = R.runDay parser part1 part2
 
 testDay :: String -> String -> Spec
-testDay = T.testDay parser part1 part2 0 0
+testDay = T.testDay parser part1 part2 7 19
 
-type Input = [Int]
+type Input = String
 
 type Output1 = Int
 type Output2 = Int
 
 parser :: String -> Input
-parser = undefined
+parser = id
 
 part1 :: Input -> Output1
-part1 = undefined
+part1 = solve 4
 
 part2 :: Input -> Output2
-part2 = undefined
+part2 = solve 14
+
+solve :: Int -> String -> Int
+solve n = (+n) . length . takeWhile ((<n) . Set.size) . map (Set.fromList . take n) . tails
