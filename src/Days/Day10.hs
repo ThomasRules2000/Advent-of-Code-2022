52025338a8e923462d742ab560b205a39e4cef63
Thomas Stacey <thomas@thomasstacey.co.uk>
2022-12-10 21:50:53 +0000

    Formatting

--- src/Days/Day10.hs
+++ src/Days/Day10.hs
@@ -1,14 +1,15 @@
 module Days.Day10 where
-import           Data.List.Split (chunksOf)
-import           Data.Matrix     (Matrix)
-import qualified Data.Matrix     as Matrix
-import qualified Program.RunDay  as R (runDay)
-import qualified Program.TestDay as T (testDay)
-import           System.Clock    (TimeSpec)
-import           Test.Hspec      (Spec)
-import qualified Util.Picture    as Picture
-import           Util.Picture    (Picture (..))
-import           Util.Util       (takeEveryN)
+import           Data.Composition ((.:))
+import           Data.List.Split  (chunksOf)
+import           Data.Matrix      (Matrix)
+import qualified Data.Matrix      as Matrix
+import qualified Program.RunDay   as R (runDay)
+import qualified Program.TestDay  as T (testDay)
+import           System.Clock     (TimeSpec)
+import           Test.Hspec       (Spec)
+import qualified Util.Picture     as Picture
+import           Util.Picture     (Picture (..))
+import           Util.Util        (takeEveryN)
 
 runDay :: String -> IO (Maybe TimeSpec, Maybe TimeSpec, Maybe TimeSpec)
 runDay = R.runDay parser part1 part2
@@ -39,7 +40,11 @@ parser = map (getInstruction . words) . lines
         getInstruction i           = error $ "Invalid Instruction: " ++ show i
 
 part1 :: Input -> Output1
-part1 = sum . zipWith (*) [20,60..] . takeEveryN 40 . drop 19 . getSeq 1
+part1 = sum 
+      . zipWith (*) [20,60..] 
+      . takeEveryN 40 
+      . drop 19 
+      . getSeq 1
 
 getSeq :: Int -> [Instruction] -> [Int]
 getSeq x []            = []
@@ -47,4 +52,8 @@ getSeq x (NoOp:is)     = x : getSeq x is
 getSeq x ((AddX n):is) = x : x : getSeq (x+n) is
 
 part2 :: Input -> Output2
-part2 = Picture . Matrix.fromLists . chunksOf 40 . zipWith (\c x -> x - 1 <= c && c <= x + 1) (cycle [0..39]) . getSeq 1
+part2 = Picture
+      . Matrix.fromLists
+      . chunksOf 40
+      . zipWith ((<=1) . abs .: (-)) (cycle [0..39])
+      . getSeq 1
