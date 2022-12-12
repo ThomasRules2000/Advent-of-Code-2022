4976107e726692df2d16e53bb86f3450b3b06831
Thomas Stacey <Thomas.Stacey@warwick.ac.uk>
2022-12-12 10:33:17 +0000

    Day 12

--- src/Days/Day12.hs
+++ src/Days/Day12.hs
@@ -1,25 +1,62 @@
 module Days.Day12 where
-import qualified Program.RunDay  as R (runDay)
-import qualified Program.TestDay as T (testDay)
-import           System.Clock    (TimeSpec)
-import           Test.Hspec      (Spec)
+import           Data.Char
+import           Data.Heap        (MinPrioHeap)
+import qualified Data.Heap        as Heap
+import           Data.Map.Strict  (Map)
+import qualified Data.Map.Strict  as Map
+import           Data.Set         (Set)
+import qualified Data.Set         as Set
+import           Data.Tuple.All   (Curry (uncurryN))
+import           Data.Tuple.Extra (second3)
+import qualified Program.RunDay   as R (runDay)
+import qualified Program.TestDay  as T (testDay)
+import           System.Clock     (TimeSpec)
+import           Test.Hspec       (Spec)
+import qualified Util.Map         as Map
 
 runDay :: String -> IO (Maybe TimeSpec, Maybe TimeSpec, Maybe TimeSpec)
 runDay = R.runDay parser part1 part2
 
 testDay :: String -> String -> Spec
-testDay = T.testDay parser part1 part2 0 0
+testDay = T.testDay parser part1 part2 31 29
 
-type Input = [Int]
+type Pos = (Int, Int)
+
+type Input = (Map Pos Int, Pos, Pos)
 
 type Output1 = Int
 type Output2 = Int
 
 parser :: String -> Input
-parser = undefined
+parser inp = (getElevation <$> grid, start, end)
+    where
+        grid = Map.fromGrid $ lines inp
+        start = head $ Map.keys $ Map.filter (=='S') grid
+        end = head $ Map.keys $ Map.filter (=='E') grid
+
+getElevation :: Char -> Int
+getElevation 'S' = 0
+getElevation 'E' = 25
+getElevation c
+    | 'a' <= c && c <= 'z' = ord c - ord 'a'
+    | otherwise = error $ "Invalid Character " <> show c
 
 part1 :: Input -> Output1
-part1 = undefined
+part1 = uncurryN (dijkstra Set.empty) . second3 Set.singleton . fmap (Heap.singleton . (0,))
+
+dijkstra :: Set Pos -> Map Pos Int -> Set Pos -> MinPrioHeap Int Pos  -> Int
+dijkstra visited elevations starts queue
+    | curr `Set.member` starts = cost
+    | curr `Set.member` visited = dijkstra visited elevations starts rest
+    | otherwise = dijkstra (Set.insert curr visited) elevations starts (foldr (\p h -> Heap.insert (cost+1, p) h) rest next)
+    where
+        Just ((cost, curr), rest) = Heap.view queue
+        elevation = elevations Map.! curr
+        next = filter (\p -> Map.member p elevations && elevations Map.! p +1 >= elevation) $ getAround curr
+
+getAround :: Pos -> [Pos]
+getAround (x, y) = [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
 
 part2 :: Input -> Output2
-part2 = undefined
+part2 (els, _, end) = dijkstra Set.empty els starts $ Heap.singleton (0, end)
+    where starts = Map.keysSet $ Map.filter (==0) els
