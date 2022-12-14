97735b9de85a69d9b21818f282655a5f1dcbb7f7
Thomas Stacey <ThomasRules2000@users.noreply.github.com>
2022-12-14 17:37:09 +0000

    Efficiency Improvements

--- src/Days/Day14.hs
+++ src/Days/Day14.hs
@@ -1,8 +1,9 @@
 module Days.Day14 where
-import           Control.Monad                    (when, unless)
-import           Control.Monad.Trans.State.Strict (State, execState, gets,
+import           Control.Monad.Extra              (ifM)
+import           Control.Monad.Trans.State.Strict (State, evalState, gets,
                                                    modify)
 import           Data.Bifunctor                   (first, second)
+import           Data.Functor                     (($>))
 import           Data.List.Split                  (splitOn)
 import           Data.Set                         (Set)
 import qualified Data.Set                         as Set
@@ -29,9 +30,8 @@ parser :: String -> Input
 parser = Set.unions . map (getRocks . map (listToTuple . map read . splitOn ",") . splitOn " -> ") . lines
 
 getRocks :: [Pos] -> Set Pos
-getRocks ((x1,y1):p2@(x2,y2):rest) = Set.union (Set.fromList newLine) $ getRocks (p2:rest)
-    where
-        newLine
+getRocks ((x1,y1):rest@((x2,y2):_)) = Set.union (Set.fromList newLine) $ getRocks rest
+    where newLine
             | x1 == x2 = [(x1,y) | y <- [y1, (y1+signum (y2-y1))..y2]]
             | otherwise = [(x,y1) | x <- [x1, (x1+signum (x2-x1))..x2]]
 getRocks _ = Set.empty
@@ -39,22 +39,20 @@ getRocks _ = Set.empty
 part1 :: Input -> Output1
 part1 = solve False
 
-numReachable :: Bool -> Set Pos -> Int -> Pos -> State (Set Pos, Bool) ()
-numReachable isFloor rocks maxY curr@(x,y)
+numReachable :: Bool -> Int -> Pos -> State (Set Pos, Bool) Int
+numReachable isFloor maxY curr@(x,y)
     -- If we go below maxY, for Part 1 we are done as this is the abyss, for Part 2 this is the floor so put the sand there
-    | y > maxY = modify $ if isFloor then first $ Set.insert curr else second $ const True
+    | y > maxY = modify (if isFloor then first $ Set.insert curr else second $ const True) $> fromEnum isFloor
     -- Otherwise, if we aren't done and we haven't already checked this square, consider the square
-    | otherwise = gets (uncurry (flip (||)) . first (curr `Set.member`)) >>= flip unless (do
+    | otherwise = ifM (gets $ uncurry (flip (||)) . first (curr `Set.member`)) (return 0) $ do
+        nextPs <- gets $ (\rocks -> filter (`Set.notMember` rocks) [(x, y+1), (x-1, y+1), (x+1, y+1)]) . fst
         -- First check the up to 3 squares the sand could move in to which aren't rocks
-        mapM_ (numReachable isFloor rocks maxY) nextPs
-        -- Get the new places where there is sand
-        sand <- gets fst
+        n <- sum <$> mapM (numReachable isFloor maxY) nextPs
         -- If there is now sand in the places where rocks weren't, put sand in the current square
-        when (all (`Set.member` sand) nextPs) $ modify $ first $ Set.insert curr)
-        where nextPs = filter (`Set.notMember` rocks) [(x, y+1), (x-1, y+1), (x+1, y+1)]
+        ifM ((`any` nextPs) <$> gets (flip Set.notMember . fst)) (return n) $ modify (first $ Set.insert curr) $> (n + 1)
 
 solve :: Bool -> Set Pos -> Int
-solve isFloor rocks = Set.size $ fst $ execState (numReachable isFloor rocks maxY (500,0)) (Set.empty, False)
+solve isFloor rocks = evalState (numReachable isFloor maxY (500,0)) (rocks, False)
     where maxY = Set.findMax $ Set.map snd rocks
 
 part2 :: Input -> Output2
